1c0c30f7bdc75766986d1e9eb2a7d14a8fc8ce25
31b4 <75566095+31b4@users.noreply.github.com>
2022-09-05 22:07:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 function ConvertIt(){
+    document.getElementById("output").innerHTML=""
     var text = document.getElementById("inp").value
-    console.log(text)
+    var output = MakeIt(text)
+    console.log(output)
+    var p = document.createElement("p")
+    p.innerHTML=output
+
+    document.getElementById("output").append(p)
 }
 
-function MakeIt(){
-
+function MakeIt(text){
+    var array = text.split(' ')
+    var output =""
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        if (element.length >3) {
+           var first_letter = element[0]
+           var last_letter = element[element.length-1]
+           var between = element.slice(1, -1)
+           var shuffled = between.split('').sort(function(){return 0.5-Math.random()}).join('')
+           output += ' '+first_letter+shuffled+last_letter 
+        }
+        else{
+            output += ' '+element
+        }
+    }
+    return output
 }

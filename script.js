5604f516286de52845780976a4a05edfe65575e4
31b4 <75566095+31b4@users.noreply.github.com>
2022-09-06 22:02:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -15,10 +15,13 @@ function MakeIt(text){
     for (let i = 0; i < array.length; i++) {
         const element = array[i];
         if (element.length >3) {
-           var first_letter = element[0]
-           var last_letter = element[element.length-1]
-           var between = element.slice(1, -1)
-           var shuffled = between.split('').sort(function(){return 0.5-Math.random()}).join('')
+            var first_letter = element[0]
+            var last_letter = element[element.length-1]
+            var between = element.slice(1, -1)
+            var shuffled = between.split('').sort(function(){return 0.5-Math.random()}).join('')
+            while (between == shuffled) {
+                shuffled = between.split('').sort(function(){return 0.5-Math.random()}).join('')
+            }
            output += ' '+first_letter+shuffled+last_letter 
         }
         else{
